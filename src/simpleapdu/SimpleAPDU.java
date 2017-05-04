d277700bf9e6b7d67af697e27d7e074aa1076a71
Ismail <ict.lotfi@gmail.com>
2017-05-04 22:40:52 +0200

    comments added to init function

--- src/simpleapdu/SimpleAPDU.java
+++ src/simpleapdu/SimpleAPDU.java
@@ -12,11 +12,15 @@ public class SimpleAPDU {
 //private static MyAPDU m_apdu;
     public static void main(String[] args) {
         try {
-            MyAPDU m_apdu= new MyAPDU();
+            	MyAPDU m_apdu= new MyAPDU();
+	    	// set the default value for the PUK
+            	m_apdu.setPUK();
 
-            m_apdu.setPUK();
-            m_apdu.executeRun();
-            m_apdu.setPin((byte)1, (byte)2, (byte)3, (byte)4);
+	    	// change the card state to setup
+            	m_apdu.executeRun();
+
+		// set a default PIN
+            	m_apdu.setPin((byte)1, (byte)2, (byte)3, (byte)4);
 
         } catch (Exception ex) {
             System.out.println("Exception : " + ex);
